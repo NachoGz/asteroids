0ed433d7a352498f75cabf3fd0f8e923a91ba7e4
NachoGz <ignaciogomezf1@gmail.com>
2025-01-08 12:21:19 -0300

    add asteroids, shooting, collision effect and splitting of asteroids when being shot

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -11,10 +14,20 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers = updatable
+
+    asteroidfield = AsteroidField()
+
+    Shot.containers = (shots, updatable, drawable)
+    
     while True:
         # if user closed the window, then break the loop
         for event in pygame.event.get():
@@ -22,12 +35,21 @@ def main():
                 return
         
         screen.fill(color="black")
-        for player in updatable:
-            player.update(dt)
-        
-        for player in drawable:
-            player.draw(screen)
+        for elem in drawable:
+            elem.draw(screen)
 
+        for elem in updatable:
+            elem.update(dt)
+        
+        for asteroid in asteroids:
+            if asteroid.check_collision(player):
+                print("Game over!")
+                return
+            for shot in shots:
+                if asteroid.check_collision(shot):
+                    shot.kill()
+                    asteroid.split()
+        
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
